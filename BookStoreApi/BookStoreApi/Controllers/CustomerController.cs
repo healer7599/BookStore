f80c664b659f81ac009f026edec801d0c5779801
yeu ha noi <43584342+healer7599@users.noreply.github.com>
2022-08-02 20:45:46 +0700

    Add files via upload

---
+++ BookStoreApi/BookStoreApi/Controllers/CustomerController.cs
@@ -0,0 +1,59 @@
+﻿using BookStoreApi.Model;
+using BookStoreBL;
+using Entity.BusinessModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        /// <summary>
+        /// Lấy danh sách nhân viên
+        /// </summary>
+        /// <param name="empID"></param>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        [Route("{searchText?}")]
+        [HttpGet]
+        public async Task<IActionResult> GetCustomer(string? searchText)
+        {
+            var result = new ServiceResult();
+
+            try
+            {
+                result.Data = new CustomerBL().GetCustomer(searchText ?? string.Empty);
+            }
+            catch (Exception ex)
+            {
+                result.Message = ex.Message;
+            }
+
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Thêm mới nhân viên
+        /// </summary>
+        /// <param name="emp"></param>
+        /// <returns></returns>
+        [HttpPost("")]
+        public async Task<IActionResult> InsertEmployee([FromBody] Customer cus)
+        {
+            var result = new ServiceResult();
+
+            try
+            {
+                result.Data = new CustomerBL().InsertCustomer(cus);
+            }
+            catch (Exception ex)
+            {
+                result.Message = ex.Message;
+            }
+
+            return Ok(result);
+        }
+    }
+}
---
+++ BookStoreApi/BookStoreApi/Controllers/EmployeeController.cs
@@ -0,0 +1,59 @@
+﻿using BookStoreApi.Model;
+using BookStoreBL;
+using Entity.BusinessModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        /// <summary>
+        /// Lấy danh sách nhân viên
+        /// </summary>
+        /// <param name="empID"></param>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        [Route("{searchText?}")]
+        [HttpGet]
+        public async Task<IActionResult> GetEmployee(string? searchText)
+        {
+            var result = new ServiceResult();
+
+            try
+            {
+                result.Data = new EmployeeBL().GetEmployee(searchText ?? string.Empty);
+            }
+            catch (Exception ex)
+            {
+                result.Message = ex.Message;
+            }
+
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Thêm mới nhân viên
+        /// </summary>
+        /// <param name="emp"></param>
+        /// <returns></returns>
+        [HttpPost("")]
+        public async Task<IActionResult> InsertEmployee([FromBody] Employee emp)
+        {
+            var result = new ServiceResult();
+
+            try
+            {
+                result.Data = new EmployeeBL().InsertEmployee(emp);
+            }
+            catch (Exception ex)
+            {
+                result.Message = ex.Message;
+            }
+
+            return Ok(result);
+        }
+    }
+}
